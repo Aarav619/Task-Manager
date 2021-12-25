e209decae2d9d1e93ad22ca7481a766d9809433b
Aarav619 <aaravmishra619@gmail.com>
2021-12-25 12:45:08 +0530

    initial commit

---
+++ task.js
@@ -0,0 +1,137 @@
+const fs = require("fs");
+const process = require("process-module");
+
+const usage_help = () => {
+  let usage = `Usage :-
+$ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX            # Delete the incomplete item with the given index
+$ ./task done INDEX           # Mark the incomplete item with the given index as complete
+$ ./task help                 # Show usage
+$ ./task report               # Statistics`;
+
+  console.log(usage);
+};
+
+const addTask = () => {
+  let task = "";
+  const path = "task.txt";
+  var result = false;
+  for (var i = 3; i < process.argv.length; ++i) {
+    task += process.argv[i] + " ";
+  }
+
+  task = task.trim();
+  var task_length = task.length;
+  var priority = process.argv[3];
+  var task_without_priority = task.slice(2, task_length);
+
+  try {
+    if (fs.existsSync(path)) {
+      result = true;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  var task_to_add = task + "\n";
+  if (result == true) {
+    fs.appendFile("task.txt", "\n" + task_to_add, function (err) {
+      if (err) {
+        return console.error("error");
+      }
+      console.log(
+        `Added task: "${task_without_priority}" with priority ${priority}`
+      );
+    });
+  }
+  if (result != true) {
+    fs.writeFile("task.txt", task_to_add, function (err) {
+      if (err) {
+        return console.error("error");
+      }
+      console.log(
+        `Added task: "${task_without_priority}" with priority ${priority}`
+      );
+    });
+  }
+};
+
+const doneTask = () => {
+  var done_task_index = process.argv[3];
+  var done_tasks = fs.readFileSync("task.txt").toString().split("\n");
+  const path = "completed.txt";
+  var result = false;
+
+  try {
+    if (fs.existsSync(path)) {
+      result = true;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  for (var i = 0; i < done_tasks.length; ++i) {
+    if (i == done_task_index) {
+      if (result != true) {
+        var to_add_task = done_tasks[i].slice(2);
+        fs.writeFile("completed.txt", to_add_task, function (err) {
+          if (err) {
+            return console.error("error");
+          }
+          console.log("Marked item as done.");
+        });
+      }
+      if (result == true) {
+        var to_add_task = done_tasks[i].slice(2);
+        fs.appendFile("completed.txt", "\n" + to_add_task, function (err) {
+          if (err) {
+            return console.error("error");
+          }
+          console.log("Marked item as done.");
+        });
+      }
+    }
+
+    if (i != done_task_index) {
+      console.log(
+        `Error: no incomplete item with index #${done_task_index} exists.`
+      );
+    }
+  }
+};
+
+function main() {
+  var argLength = process.argv.length;
+  const argument = process.argv[2];
+
+  if (argument === "help") {
+    usage_help();
+  }
+
+  if (argument === "add") {
+    if (argLength == 3) {
+      console.log("Error: Missing tasks string. Nothing added!");
+    }
+
+    if (argLength > 3) {
+      addTask();
+    }
+  }
+
+  if (argument === "done") {
+    if (argLength == 3) {
+      console.log("Error: Missing NUMBER for marking tasks as done.");
+    }
+
+    if (argLength > 3) {
+      doneTask();
+    }
+  }
+
+  if (argLength < 3) {
+    usage_help();
+  }
+}
+
+main();
