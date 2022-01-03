e6c2aa4e8e7a7edef8dc8c33b266fd602a5be1be
Aarav619 <aaravmishra619@gmail.com>
2022-01-03 13:29:35 +0530

    lint code

--- task.js
+++ task.js
@@ -52,10 +52,8 @@ const addTask = (result) => {
 	task = task.trim();
 	var task_length = task.length;
 	var priority = process.argv[3];
-
 	var task_without_priority = task.slice(2, task_length);
 	var task_to_add = task;
-
 	if (result == true) {
 		fs.appendFile("task.txt", "\n" + task_to_add, function (err) {
 			if (err) {
@@ -80,7 +78,6 @@ const addTask = (result) => {
 
 const completedTask = (result, done_tasks) => {
 	var done_task_index = process.argv[3];
-
 	if (result == true) {
 		done_tasks.sort();
 		for (var i = 1; i < done_tasks.length + 1; ++i) {
@@ -96,7 +93,7 @@ const completedTask = (result, done_tasks) => {
 				}
 				if (result == true) {
 					var to_add_task = done_tasks[i - 1].slice(2);
-					fs.appendFile("completed.txt", "\n" + to_add_task , function (err) {
+					fs.appendFile("completed.txt","\n" + to_add_task, function (err) {
 							if (err) {
 								return console.error("error");
 							}
@@ -106,18 +103,18 @@ const completedTask = (result, done_tasks) => {
 				}
 			}
 		}
-		if (done_task_index != done_tasks.length && done_task_index > done_tasks.length) {
-			console.log(`Error: no incomplete item with index #${done_task_index} exists.`);
+		if (done_task_index != done_tasks.length &&done_task_index > done_tasks.length) {
+			console.log(
+				`Error: no incomplete item with index #${done_task_index} exists.`
+			);
 		}
 	}
 };
 
 const lsTask = (result, all_task) => {
 	var all_task_list = new Array();
-
 	if (result == true) {
 		all_task.sort();
-
 		for (var i = 0; i < all_task.length; ++i) {
 			var all_task_without_index = all_task[i].slice(2);
 			all_task_list.push(all_task_without_index);
@@ -145,7 +142,6 @@ const reportTask = (result1, result2, tasks, completed_tasks) => {
 				(x) => !completed_tasks.includes(x)
 			);
 			console.log("Pending :", incomplete_tasks.length);
-
 			for (var i = 0; i < incomplete_tasks.length; ++i) {
 				console.log(`${i + 1}. ` + incomplete_tasks[i] + ` [${i + 1}]`);
 			}
@@ -161,7 +157,9 @@ const reportTask = (result1, result2, tasks, completed_tasks) => {
 			}
 			console.log("Pending :", intermediate_tasks.length);
 			for (var i = 0; i < intermediate_tasks.length; ++i) {
-				console.log(`${i + 1}. ` + intermediate_tasks[i] + ` [${i + 1}]`);
+				console.log(
+					`${i + 1}. ` + intermediate_tasks[i] + ` [${i + 1}]`
+				);
 			}
 			console.log("Comepleted : 0");
 		}
@@ -171,24 +169,22 @@ const reportTask = (result1, result2, tasks, completed_tasks) => {
 const delTask = (result, all_task_list) => {
 	var del_index = process.argv[3];
 	if (result == true) {
-		for (var i = 0; i < all_task_list.length; ++i) {
-			if (i + 1 == del_index) {
-				all_task_list.splice(i, 1);
-				console.log(`Deleted task #${del_index}`);
-			}
-		}
-		for (var i = 0; i < all_task_list.length; ++i) {
-			fs.writeFile("task.txt", all_task_list[i] + "\n", function (err) {
-				if (err) {
-					return console.error("error");
-				}
-			});
-		}
-		if (del_index != all_task_list.length && del_index > all_task_list.length) {
-			console.log(
+		if (del_index > all_task_list.length || all_task_list[0] == "") {
+			return console.log(
 				`Error: task with index #${del_index} does not exist. Nothing deleted.`
 			);
 		}
+		for (var i = 0; i < all_task_list.length; i++) {
+			if (i + 1 == del_index) {
+				all_task_list.splice(i,1);
+				console.log(`Deleted task #${del_index}`);
+			}
+		}
+		fs.writeFile("task.txt", all_task_list.join("\n"), function (err) {
+			if (err) {
+				return console.error("error");
+			}
+		});
 	}
 };
 
@@ -198,7 +194,8 @@ function main() {
 
 	if (argument === "help") {
 		usage_help();
-	} else if (argument === "add") {
+	} 
+	else if (argument === "add") {
 		if (argLength == 3) {
 			console.log("Error: Missing tasks string. Nothing added!");
 		}
@@ -207,7 +204,8 @@ function main() {
 			var result = verifyPath(path);
 			addTask(result);
 		}
-	} else if (argument === "done") {
+	} 
+	else if (argument === "done") {
 		if (argLength == 3) {
 			console.log("Error: Missing NUMBER for marking tasks as done.");
 		}
@@ -217,12 +215,14 @@ function main() {
 			var done_tasks = readTasks(path);
 			completedTask(result, done_tasks);
 		}
-	} else if (argument === "ls") {
+	} 
+	else if (argument === "ls") {
 		const path = "task.txt";
 		const result = verifyPath(path);
 		var all_tasks = readTasks(path);
 		lsTask(result, all_tasks);
-	} else if (argument === "report") {
+	} 
+	else if (argument === "report") {
 		path1 = "task.txt";
 		path2 = "completed.txt";
 		result1 = verifyPath(path1);
@@ -230,7 +230,8 @@ function main() {
 		var tasks = readTasks(path1);
 		var completed_tasks = readTasks(path2);
 		reportTask(result1, result2, tasks, completed_tasks);
-	} else if (argument === "del") {
+	} 
+	else if (argument === "del") {
 		if (argLength == 3) {
 			console.log("Error: Missing NUMBER for deleting tasks.");
 		}
@@ -240,12 +241,12 @@ function main() {
 			var all_task_list = readTasks(path);
 			delTask(result, all_task_list);
 		}
-	} else if (argLength < 3) {
+	}
+	 else if (argLength < 3) {
 		usage_help();
-	} else {
-		console.log(
-			`Invalid command ${argument}, Select the appropiate command from menu below: \n`
-		);
+	}
+	 else {
+		console.log(`Invalid command ${argument}, Select the appropiate command from menu below: \n`);
 		usage_help();
 	}
 }
