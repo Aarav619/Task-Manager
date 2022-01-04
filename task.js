e555ff02b9d866d6dc9de862b6800ffff1662277
Aarav619 <aaravmishra619@gmail.com>
2022-01-04 20:09:54 +0530

    format code

--- task.js
+++ task.js
@@ -5,16 +5,16 @@ const add = require("./lib/addTask");
 const completed = require("./lib/completedTask");
 const ls = require("./lib/lsTask");
 const report = require("./lib/reportTask");
-const del = require("./lib/delTask")
-const verify = require("./lib/verifyPath")
+const del = require("./lib/delTask");
+const verify = require("./lib/verifyPath");
 
 function main() {
 	var argLength = process.argv.length;
 	const argument = process.argv[2];
 
 	if (argument === "help") {
-		help.usage_help()
-	} 
+		help.usage_help();
+	}
 	else if (argument === "add") {
 		if (argLength == 3) {
 			console.log("Error: Missing tasks string. Nothing added!");
@@ -24,21 +24,21 @@ function main() {
 			var result = verify.verifyPath(path);
 			add.addTask(result);
 		}
-	} 
+	}
 	else if (argument === "done") {
 		if (argLength == 3) {
 			console.log("Error: Missing NUMBER for marking tasks as done.");
 		}
 		if (argLength > 3) {
 			const path1 = "task.txt";
-			const path2 = "completed.txt"
+			const path2 = "completed.txt";
 			property = "done";
 			var result1 = verify.verifyPath(path1);
-			var result2 = verify.verifyPath(path2)
+			var result2 = verify.verifyPath(path2);
 			var done_tasks = read.readTasks(path1, property);
 			completed.completedTask(result1, result2, done_tasks);
 		}
-	} 
+	}
 	else if (argument === "ls") {
 		const path = "task.txt";
 		const result = verify.verifyPath(path);
@@ -62,16 +62,18 @@ function main() {
 		if (argLength == 4) {
 			const path = "task.txt";
 			const result = verify.verifyPath(path);
-			property = "del"
+			property = "del";
 			var all_task_list = read.readTasks(path, property);
 			del.delTask(result, all_task_list);
 		}
 	}
-	 else if (argLength < 3) {
+	else if (argLength < 3) {
 		help.usage_help();
-	}
-	 else {
-		console.log(`Invalid command ${argument}, Select the appropriate command from menu below: \n`);
+	} 
+	else {
+		console.log(
+			`Invalid command ${argument}, Select the appropriate command from menu below: \n`
+		);
 		help.usage_help();
 	}
 }
